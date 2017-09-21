7e0b998cb621ce6db8190d9a70608a365507cde4
Baize <baize.hex66ccff@gmail.com>
2017-09-21 11:26:04 +0800

    Builder

---
+++ Builder/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class Student
+    {
+        private string name;
+        private int age;
+        private string address;
+        private string password;
+        public Student() { }
+        public Student(Student origin)
+        {
+            name = origin.name;
+            age = origin.age;
+            address = origin.address;
+            password = origin.password;
+        }
+        public Student(string _name,int _age,string _address,string _ps)
+        {
+            name = _name;
+            age = _age;
+            address = _address;
+            password = _ps;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string Passward
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
---
+++ Builder/StudentBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    class StudentBuilder
+    {
+        private Student student;
+
+        public StudentBuilder()
+        {
+            student = new Student();
+        }
+        public void BuildName(string name)
+        {
+            student.Name = name;            
+        }
+        public void BuildAge(int age)
+        {
+            student.Age = age;            
+        }
+        public void BuildAddress(string address)
+        {
+            student.Address = address;            
+        }
+        public void BuildPassward(string passward)
+        {
+            student.Passward = passward;            
+        }
+        public Student GetStudent()
+        {
+            return new Student(student);
+        }
+    }
+}
