d3303730200ac3332b0d5cb67f9f02e44d92b632
Baize <hejingjun0102@qq.com>
2019-04-13 15:09:16 +0800

    add abstractfactory example2

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -10,20 +10,21 @@ namespace AbstractFactory
     {
         static void Main(string[] args)
         {
-            Creator BlacksmithInBaseTown = new BlacksmithInBaseTown();
-            Creator BlacksmithInForest = new BlacksmithInForest();
-            Sword SwordOfBlacksmithInBaseTown = BlacksmithInBaseTown.CreatSword();
-            Shield ShieldOfBlacksmithInBaseTown = BlacksmithInBaseTown.CreatShield();
-            Sword SwordOfBlacksmithInForest = BlacksmithInForest.CreatSword();
-            Shield ShieldOfBlacksmithInForest = BlacksmithInForest.CreatShield();
+            //Creator BlacksmithInBaseTown = new BlacksmithInBaseTown();
+            //Creator BlacksmithInForest = new BlacksmithInForest();
+            //Sword SwordOfBlacksmithInBaseTown = BlacksmithInBaseTown.CreatSword();
+            //Shield ShieldOfBlacksmithInBaseTown = BlacksmithInBaseTown.CreatShield();
+            //Sword SwordOfBlacksmithInForest = BlacksmithInForest.CreatSword();
+            //Shield ShieldOfBlacksmithInForest = BlacksmithInForest.CreatShield();
 
-            Sword SwordOfMaster = BlacksmithInForest.CreatSword();
+            //Sword SwordOfMaster = BlacksmithInForest.CreatSword();
 
-            SwordOfBlacksmithInBaseTown.PrintInfo();
-            SwordOfBlacksmithInForest.PrintInfo();
-            ShieldOfBlacksmithInBaseTown.PrintInfo();
-            ShieldOfBlacksmithInForest.PrintInfo();
-
+            //SwordOfBlacksmithInBaseTown.PrintInfo();
+            //SwordOfBlacksmithInForest.PrintInfo();
+            //ShieldOfBlacksmithInBaseTown.PrintInfo();
+            //ShieldOfBlacksmithInForest.PrintInfo();
+            AbstractFactoryExample2 exp2 = new AbstractFactoryExample2();
+            exp2.RunExample2();
             
             Console.Read();
 
